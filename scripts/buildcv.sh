3b1765228fdf65400f6a70459e999b1eb67a64c0
Pierre FORCE <pforce@arkoon.dev>
2019-08-26 17:09:34 +0200

    Change path on scripts/convert2html.sh and scripts/buildcv.sh

--- scripts/buildcv.sh
+++ scripts/buildcv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-SCRIPT_PATH=$(pwd)
+SCRIPT_PATH="$(git rev-parse --show-toplevel)"
 
 ${SCRIPT_PATH}/scripts/convert2pdf.sh
 ${SCRIPT_PATH}/scripts/convert2html.sh
--- scripts/convert2html.sh
+++ scripts/convert2html.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pdffile=cv_pierre.force.pdf
-pdfdir="/home/pierre/PERSO/CV/out"
+pdfdir="$(git rev-parse --show-toplevel)/out"
 
 
 docker run -ti --rm -v ${pdfdir}:/pdf bwits/pdf2htmlex pdf2htmlEX --zoom 1.3 ${pdffile}
