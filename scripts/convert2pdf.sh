a5f3e99cf473999bbb22f73ef739887fcf48664a
Pierre FORCE <pforce@arkoon.dev>
2019-08-26 14:54:19 +0200

    Change scripts/convert2pdf.sh script to work with dockerhub

--- scripts/convert2pdf.sh
+++ scripts/convert2pdf.sh
@@ -4,5 +4,5 @@ CV_DIR_HOST="/home/pierre/PERSO/CV"
 CV_DIR_GUEST="/home/cv"
 
 
-docker rm cvmaker
-docker run -it --rm -v "${CV_DIR_HOST}:${CV_DIR_GUEST}" --entrypoint "docker/entrypoint.sh" cvmaker 
+#docker rm sixc/cvmaker 
+docker run -it --rm -v "${CV_DIR_HOST}:${CV_DIR_GUEST}" --entrypoint "docker/entrypoint.sh" sixc/cvmaker 
