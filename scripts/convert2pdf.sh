288b88d128821698548fb687634e9497f3309a33
Pierre FORCE <pforce@arkoon.dev>
2019-08-26 17:04:32 +0200

    Change path on scripts/convert2pdf.sh

--- scripts/convert2pdf.sh
+++ scripts/convert2pdf.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-CV_DIR_HOST="/home/pierre/PERSO/CV"
+CV_DIR_HOST="$(git rev-parse --show-toplevel)"
 CV_DIR_GUEST="/home/cv"
 
-
-#docker rm sixc/cvmaker 
 docker run -it --rm -v "${CV_DIR_HOST}:${CV_DIR_GUEST}" --entrypoint "docker/entrypoint.sh" sixc/cvmaker 
