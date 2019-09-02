cc35687df6645218bf56cb1fab84ecba10320a91
Pierre FORCE <pforce@arkoon.dev>
2019-09-02 13:48:07 +0200

    Change Convert2pdf to view build log during execution

--- scripts/convert2pdf.sh
+++ scripts/convert2pdf.sh
@@ -4,4 +4,4 @@ CV_DIR_HOST="$(git rev-parse --show-toplevel)"
 CV_DIR_GUEST="/home/cv"
 TEX=$1
 
-docker run -di --rm -v "${CV_DIR_HOST}:${CV_DIR_GUEST}" --entrypoint "docker/entrypoint.sh" --env TEXFILE=${TEX} sixc/cvmaker
+docker run -ti --rm -v "${CV_DIR_HOST}:${CV_DIR_GUEST}" --entrypoint "docker/entrypoint.sh" --env TEXFILE=${TEX} sixc/cvmaker
